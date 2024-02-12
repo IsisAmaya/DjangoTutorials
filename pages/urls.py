8f4d38c7f0132c4cd2bec9ec79b9f12f6e311db0
IsisAmaya <icamayaa@eafit.edu.co>
2024-02-11 21:45:15 -0500

    funcionalidad para agregar nuevos productos

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path('about/', AboutPageView.as_view(), name='about'),
     path('products/', ProductIndexView.as_view(), name='index'),
+    path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
 ]
