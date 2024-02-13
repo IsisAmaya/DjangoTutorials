c0ac9a00f937952fe2bfc17d095da4c0d7e2eebe
IsisAmaya <icamayaa@eafit.edu.co>
2024-02-12 20:47:04 -0500

    actividad 1 completada

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
--- pages/views.py
+++ pages/views.py
@@ -10,16 +10,27 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Your Name",
+        })
+        return context
 
-def get_context_data(self, **kwargs):
-    context = super.get_context_data(**kwargs)
-    context.update({
-        "title": "About us - Online Store",
-        "subtitle": "About us",
-        "description": "This is an about page ...",
-        "author": "Developed by: Your Name",
-    })
-    return context
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "Contact us - Online Store",
+            "subtitle": "Contact us",
+        })
+        return context
 
 
 class Product:
